cb3f2d761c1d2ab57a1c15beab13685022824794
Emerson Teixeira <emerson@exemplo.com>
2025-05-21 20:40:43 -0300

    Config automação pythonanywhere

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from order.views import welcome_view
+from bookstore import views
 
 urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
@@ -28,4 +29,6 @@ urlpatterns = [
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
+    path("update_server/", views.update, name="update"),
+    path("hello/", views.hello_world, name="hello_world")
 ]
